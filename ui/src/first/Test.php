bc82d61bfe6aaead71a76c5701cfd7151f26eada
PLcode-Be <77277636+PLcode-Be@users.noreply.github.com>
2021-01-12 14:36:43 +0900

    Add files via upload

--- ui/src/first/Test.php
+++ ui/src/first/Test.php
@@ -5,8 +5,6 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
-use pocketmine\item\Item;
-use pocketmine\Player;
 class Test extends PluginBase implements Listener{
 	public function onCommand(CommandSender $player,Command $command,string $label,array $args): bool{
 	switch ($command->getName()){
